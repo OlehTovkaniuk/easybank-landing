55596f939ae7ad3a140e4a6579c1c951e36b5941
OlehTovkaniuk <tovkaniuk@gmail.com>
2020-10-26 20:25:25 +0200

    js menu

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
-console.log(123)
+// Menu
+
+const navList = document.querySelector('.menu-list');
+const menuBtn = document.querySelector('.toggler');
+const closeBtn = document.querySelector('.close');
+const body = document.querySelector('body');
+
+menuBtn.addEventListener('click', () => {
+    menuBtn.classList.add('leave');
+    navList.classList.add('arrive');
+    closeBtn.classList.add('arrive');
+    body.classList.add('overlay');
+});
+
+closeBtn.addEventListener('click', () => {
+    menuBtn.classList.remove('leave');
+    navList.classList.remove('arrive');
+    closeBtn.classList.remove('arrive');
+    body.classList.remove('overlay');
+});
+
