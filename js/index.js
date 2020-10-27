3be24842a53ee13653731c47f0f786f73777f68e
OlehTovkaniuk <tovkaniuk@gmail.com>
2020-10-27 11:03:46 +0200

    showcase adaptivity version

--- js/index.js
+++ js/index.js
@@ -4,18 +4,21 @@ const navList = document.querySelector('.menu-list');
 const menuBtn = document.querySelector('.toggler');
 const closeBtn = document.querySelector('.close');
 const body = document.querySelector('body');
+const overlay = document.querySelector('.overlay');
 
 menuBtn.addEventListener('click', () => {
     menuBtn.classList.add('leave');
     navList.classList.add('arrive');
     closeBtn.classList.add('arrive');
-    body.classList.add('overlay');
+    body.classList.add('overlay-active');
+    overlay.classList.add('active')
 });
 
 closeBtn.addEventListener('click', () => {
     menuBtn.classList.remove('leave');
     navList.classList.remove('arrive');
     closeBtn.classList.remove('arrive');
-    body.classList.remove('overlay');
+    body.classList.remove('overlay-active');
+    overlay.classList.remove('active')
 });
 
